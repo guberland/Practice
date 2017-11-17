c0c822a75f482323e2510ddc4ada519510e25dcb
guberland <chaojiang1189@gmail.com>
2017-11-16 16:26:03 -0800

    test case for catalog app

--- DemoSite (Django)/catalogs/tests.py
+++ DemoSite (Django)/catalogs/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
-
+from django.utils import timezone
 # Create your tests here.
+
+from .models import Catalog,Procedure
+
+class CatalogModelTests(TestCase):
+    def test_catalog_creation(self):
+        catalog = Catalog.objects.create(
+            name="Test how Django unchained himself",
+            description="learn to write some code will ya "
+            )
+        now = timezone.now()
+        self.assertLessEqual(catalog.created_at,now)
+        
+class ProcedureModelTests(TestCase):
+    def test_Procedure_creation(self):
+
+        procedure=Procedure.objects.create(
+            title="2nd test",
+            description="learn to write some code will ya ",
+            content="d",
+            order=0,
+            catalog=Catalog(),
+            )
+        now = timezone.now()
+        self.assertLessEqual(procedure.created_at,now)       
